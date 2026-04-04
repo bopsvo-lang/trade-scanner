b12e08440d69f21f33e5bf19b63f86be2a06bca8
bopsvo-lang <bo.ps.vo@gmail.com>
2026-04-05 04:11:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ PUMP_SCAN_SETTINGS = {
     
     # Новые настройки для WebSocket
     'websocket_top_pairs': 200,                   # Сколько пар в WebSocket
-    'shitcoin_volume_threshold': 100_000,        # Объем < 0.5M$ = щиткоин
+    'shitcoin_volume_threshold': 100_000_000,        # Объем < 0.5M$ = щиткоин
     'websocket_reconnect_delay': 5,               # Задержка перед переподключением
 }
 
